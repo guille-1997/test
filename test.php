c500a308e96f18dddcce0fa723f8d6f22bb60fc2
guille-1997 <guille-1997@hotmail.com>
2017-07-04 20:03:42 -0500

    Create test.php

---
+++ test.php
@@ -0,0 +1,38 @@
+<?php
+
+class test {
+
+   /**
+	* Returns plan discount.
+	*
+	* @param $param;
+	* @return float
+	*/
+	public function test($param)
+	{
+		return '';
+	}
+
+
+   /**
+	* @return float
+	*/
+	public function test2()
+	{
+		return '';
+	}
+
+
+   /**
+	* Returns plan discount.
+	*
+	* @throws \Exception 
+	* @return float
+	*/
+	public function test3()
+	{
+		return '';
+	}
+
+
+}
